2b75e424a57d7249a57b1bc832f6e4de6ed2bd0b
Unknown <reperez79@gmail.com>
2017-09-16 12:37:15 -0700

    Reupload of PUBG App for hopefully the last time

---
+++ PUBGApp/PUBGApp/PUBGApp/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PUBGApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Click(object sender, EventArgs e)
+        {
+            
+            var button = (Button)sender;
+            button.Text = $"Good for you! Add me on steam @Pixel Amp!";
+
+        }
+    }
+}
