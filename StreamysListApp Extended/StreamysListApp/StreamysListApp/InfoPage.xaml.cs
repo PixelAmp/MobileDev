ef553a844d6edf9948b737c8f3e232871d600ca8
Unknown <reperez79@gmail.com>
2017-10-06 18:23:04 -0700

    Added a Button to the more info page to open webrowser

--- StreamysListApp Extended/StreamysListApp/StreamysListApp/InfoPage.xaml.cs
+++ StreamysListApp Extended/StreamysListApp/StreamysListApp/InfoPage.xaml.cs
@@ -12,11 +12,17 @@ namespace StreamysListApp
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class InfoPage : ContentPage
 	{
+        string url;
         public InfoPage(ImageCellTemplate CellInfo)
         {
             InitializeComponent();
             BindingContext = CellInfo;
+            url = CellInfo.URL;
         }
 
-	}
+        void NavigateToChannel(object sender, System.EventArgs e)
+        {
+            Device.OpenUri(new Uri(url));
+        }
+    }
 }
