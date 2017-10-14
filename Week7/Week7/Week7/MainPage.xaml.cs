9f8f3fa842b9e33a963b4637cb21d41bbfebc3cb
Unknown <reperez79@gmail.com>
2017-10-13 19:52:56 -0700

    Week 7 project, working with Styles

---
+++ Week7/Week7/Week7/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Week7
+{
+
+    //find out how to change the resource dictionary items
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void DetailClicked(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            
+        }
+        private void TitleClicked(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+
+
+        }
+        private void BGClicked(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+
+            
+        }
+    }
+}
