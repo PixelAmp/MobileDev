ea8c928b38b17c91706a30ee7b5701ddd594dfb5
Unknown <reperez79@gmail.com>
2017-10-18 04:19:52 -0700

    still non functioning microcharts

--- MicrochartsDemo/MicrochartsDemo/MicrochartsDemo/MainPage.xaml.cs
+++ MicrochartsDemo/MicrochartsDemo/MicrochartsDemo/MainPage.xaml.cs
@@ -5,13 +5,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
-using Entry = Microcharts.Entry;
+
+
 
 namespace MicrochartsDemo
 {
+    
     public partial class MainPage : ContentPage
     {
-        List<Entry> entries = new List<Entry>
+        ///*
+        List<Microcharts.Entry> entries = new List<Microcharts.Entry>
         {
             new Entry(200)
             {
@@ -35,21 +38,39 @@ namespace MicrochartsDemo
             }
 
         };
-        void butts()
+        //*/
+
+
+        /*
+        protected override void OnAppearing()
         {
             var entries = new[]
-    {
+{
                 new Entry(200)
                 {
                     Label = "January",
                     ValueLabel = "200",
                     FillColor = SKColor.Parse("#266489")
+                },
+                new Entry(400)
+                {
+                    Label = "February",
+                    ValueLabel = "400",
+                    FillColor = SKColor.Parse("#68B9C0")
+                },
+                new Entry(-100)
+                {
+                    Label = "March",
+                    ValueLabel = "-100",
+                    FillColor = SKColor.Parse("#90D585")
                 }
             };
 
             var chart = new BarChart() { Entries = entries };
+            //var chart2 = new LineChart() { Entries = entries };
+            this.chartView.Chart = chart;
         }
-
+        /*
 
         public MainPage()
         {
