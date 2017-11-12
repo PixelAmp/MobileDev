5ca70c696004f6af7a73169d4f265fd0f723dad2
Unknown <reperez79@gmail.com>
2017-11-12 02:17:54 -0800

    Added New MVVM projects that use API's, Removed Useless Projects

---
+++ PokedexAPI/PokedexAPI/PokedexAPI/Models/PokedexModel.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+//this file hos the model for the data recived from the Pokemon API
+//data is recived, translated using JASON and then attached to their approptarete tags
+
+namespace PokedexAPI.Models
+{
+    public static class PokedexModel
+    {
+        public partial class PokedexItem
+        {
+            //returns the Nat Dex #
+            [JsonProperty("id")]
+            public string DexNum { get; set; }
+
+            //returns the name of the pokemon
+            [JsonProperty("name")]
+            public string Name { get; set; }
+
+            //returns the chance of this Pokémon being female, in eighths; or -1 for genderless
+            [JsonProperty("gender_rate")]
+            public int GenderRate { get; set; }
+
+            //returns the base capture rate; up to 255. The higher the number, the easier the catch.
+            [JsonProperty("capture_rate")]
+            public int CapRate { get; set; }
+
+            //returns the base experience gained for defeating this Pokémon
+            [JsonProperty("base_experience")]
+            public int BaseEXP { get; set; }
+
+            //returns the base height
+            [JsonProperty("height")]
+            public int Height { get; set; }
+
+            //returns the weight
+            [JsonProperty("weight")]
+            public string Weight { get; set; }
+
+          
+
+            //returns amount of types and their values
+            [JsonProperty("types")] 
+            public PokemonType PokemonType { get; set; }
+
+
+            //returns amount of types and their values
+            [JsonProperty("stats")]
+            public Stats Stats { get; set; }
+
+            //returns amount of types and their values
+            [JsonProperty("abilities")]
+            public Abilities Abilities { get; set; }
+
+        }
+
+        public partial class PokemonType //holds the actual type of the pokemon
+        {
+            [JsonProperty("slot")]
+            public int Slot { get; set; }
+
+            [JsonProperty("name")]
+            public string Name { get; set; }
+        }
+
+        public partial class Abilities //holds the stats of the pokemon
+        {
+            [JsonProperty("slot")]
+            public int Slot { get; set; }
+
+            [JsonProperty("name")]
+            public string Name { get; set; }
+        }
+
+
+        public partial class Stats //the top of stats, EV and Base Stat
+        {
+            //[JsonProperty("stat")]
+            //public Stat Stat { get; set; }
+
+                //if this works then I don't need code above that calls function below
+            [JsonProperty("name")]
+            public string StatType { get; set; }
+
+
+            [JsonProperty("effort")]
+            public int EV { get; set; }
+
+            [JsonProperty("base_stat")]
+            public int BaseStat { get; set; }
+        }
+
+        public partial class Stat //Name of the stat
+        {
+            [JsonProperty("name")]
+            public string StatType { get; set; }
+
+        }
+
+
+
+
+
+        public partial class PokedexItem
+        {
+            public static PokedexItem FromJson(string json) => JsonConvert.DeserializeObject<PokedexItem>(json, Converter.Settings);
+        }
+
+        public static string ToJson(this PokedexItem self) => JsonConvert.SerializeObject(self, Converter.Settings);
+
+        public class Converter
+        {
+            public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+            {
+                MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+                DateParseHandling = DateParseHandling.None,
+            };
+        }
+    }
+}
---
+++ PokedexAPI/PokedexAPI/PokedexAPI/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,112 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using static PokedexAPI.Models.PokedexModel;
+using System.Runtime.CompilerServices;
+
+namespace PokedexAPI.ViewModels
+{
+    public class MainPageViewModel : BindableBase, INavigationAware
+    {
+        public DelegateCommand GetPokemonCommand { get; set; }
+        public DelegateCommand<PokedexItem> NavToMoreInfoPageCommand { get; set; }
+
+        private string _title; // this is only used by the default thing, could probably delete/remove
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        private string _buttonText; 
+        public string ButtonText
+        {
+            get { return _buttonText; }
+            set { SetProperty(ref _buttonText, value); }
+        }
+
+        private string _pokemonEnteredByUser; //hold the user euntery
+        public string PokemonEnteredByUser
+        {
+            get { return _pokemonEnteredByUser; }
+            set { SetProperty(ref _pokemonEnteredByUser, value); }
+        }
+
+        private ObservableCollection<PokedexItem> _pokedexCollection = new ObservableCollection<PokedexItem>();
+        public ObservableCollection<PokedexItem> PokedexCollection
+        {
+            get { return _pokedexCollection; }
+            set { SetProperty(ref _pokedexCollection, value); }
+        }
+
+        INavigationService _navigationService; //provides page navigation for View Models
+
+
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            //I think that this section just waits for commands to occur and sends the commands to the appropriate event
+            _navigationService = navigationService;
+
+            GetPokemonCommand = new DelegateCommand(GetPokemon);
+            NavToMoreInfoPageCommand = new DelegateCommand<PokedexItem>(NavToMoreInfoPage);
+
+        }
+
+        //navigate to new page
+        //sends all dta data currently in the model to the new page
+        private async void NavToMoreInfoPage(PokedexItem pokedexItem)
+        {
+            var navParams = new NavigationParameters();
+            navParams.Add("PokedexItemInfo", pokedexItem);
+            await _navigationService.NavigateAsync("MoreInfoPage", navParams);
+        }
+
+
+        internal async void GetPokemon()
+        {
+
+            HttpClient client = new HttpClient(); //makes new accesable HTTP client
+
+            //calls website, sending the user entry, website responds and sends back data
+            var uri = new Uri(string.Format( $"http://pokeapi.co/api/v2/pokemon/{PokemonEnteredByUser}/")); 
+            var response = await client.GetAsync(uri);
+
+            PokedexItem PokedexData = null;
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                PokedexData = PokedexItem.FromJson(content);
+            }
+            PokedexCollection.Add(PokedexData); //adds recived data to collection
+        }
+
+        private async void NavToNewPage()
+        {
+            var navParams = new NavigationParameters();
+            navParams.Add("NavFromPage", "MainPageViewModel");
+            await _navigationService.NavigateAsync("SamplePageForNavigation", navParams);
+        }
+
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+
+        }
+    }
+}
